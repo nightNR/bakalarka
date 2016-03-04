90a0b8bf094e230c8cb05c0aae890296ab095338
Peter Balaz <nightnr@gmail.com>
2016-03-05 00:35:46 +0100

    Added most common actions and routes

--- src/PubLeashBundle/Controller/PublicationController.php
+++ src/PubLeashBundle/Controller/PublicationController.php
@@ -9,6 +9,7 @@
 namespace PubLeashBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -17,7 +18,7 @@ use Symfony\Component\HttpFoundation\Response;
 class PublicationController extends Controller
 {
     /**
-     * @Route("/publications/{page}", defaults={"page": 1}, requirements={"page": "\d+"})
+     * @Route("/publication/list/{page}", defaults={"page": "1"}, requirements={"page": "\d+"})
      */
     public function publicationAction($page)
     {
@@ -26,11 +27,79 @@ class PublicationController extends Controller
     }
 
     /**
-     * @Route("/publication/{publicationName}")
+     * @Route("/publication/{publicationId}/read/chapter/list/{page}", defaults={"page": "1"}, requirements={"publicationId": "\d+", "page": "\d+"})
      */
-    public function publicationShowAction($publicationName)
+    public function showPublicationChaptersAction($publicationId, $page)
     {
-        var_dump($publicationName);
+
+    }
+
+    /**
+     * @Route("/publication/{publicationId}/read/chapter/{chapterId}", defaults={"chapterId": "1"}, requirements={"publicationId": "\d+", "chapterId": "\d+"})
+     */
+    public function showPublicationChapterAction($publicationId, $chapterId)
+    {
+
+    }
+
+    /**
+     * @Route("/publication/read/{publicationId}")
+     */
+    public function showPublicationAction($publicationId)
+    {
+        var_dump($publicationId);
         return new Response();
     }
+
+    /**
+     * @Route("/publication/review/{publicationId}/")
+     * @Method("POST")
+     * @param $publicationId
+     * @return Response
+     */
+    public function ratePublicationAction($publicationId)
+    {
+        echo "Review response";
+        return new Response();
+    }
+
+    /**
+     * @Route("/publication/{publicationId}/reviews/{page}", defaults={"page": "1"}, requirements={"publicationId": "\d+", "page": "\d+"})
+     * @param $publicationId
+     * @return Response
+     */
+    public function showReviewsAction($publicationId, $page)
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/publication/review/{reviewId}", requirements={"reviewId": "\d+"})
+     * @Method("GET")
+     * @param $reviewId
+     * @return Response
+     */
+    public function showReviewAction($reviewId)
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route(/publication/add)
+     * @return Response
+     */
+    public function addPublicationAction()
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/publication/{publicationId}/chapter/add")
+     * @param $publicationId
+     */
+    public function addChapter($publicationId)
+    {
+
+    }
+
 }
